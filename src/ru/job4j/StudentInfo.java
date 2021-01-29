1488a8cf357cf2240baa95f56810eea35ba1c3ea
tarasu4ok <taras.kramarenko@gmail.com>
2021-01-29 20:16:57 +0700

    3.3. Разбор первой программы. [#457924]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Taras Kramarenko");
+        System.out.println("09.07.1981");
+    }
+}
