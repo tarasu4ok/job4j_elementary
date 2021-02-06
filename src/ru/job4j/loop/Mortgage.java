401e74aa130e2840f1988e3e5aa3b1fc43775baa
tarasu4ok <taras.kramarenko@gmail.com>
2021-02-06 12:39:52 +0700

    5.7. Ипотека [#457963]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -2,9 +2,9 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
-        int year = 1;
+        int year = 0;
         double restAmount = amount;
-        while (restAmount + restAmount * percent / 100 > salary) {
+        while (restAmount > 0) {
             year++;
             restAmount = restAmount + restAmount  * percent / 100 - salary;
         }
